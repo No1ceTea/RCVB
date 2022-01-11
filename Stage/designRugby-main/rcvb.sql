592efae1d156c429c2ec21be97738570b9d0aa71
Ronan <ronantheveu@gmail.com>
2022-01-11 16:49:31 +0100

    ajout des diagrammes (mcd et utilisation) + mise à jour BDD

--- Stage/designRugby-main/rcvb.sql
+++ Stage/designRugby-main/rcvb.sql
@@ -1,27 +1,35 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Feb 17, 2021 at 05:53 PM
--- Server version: 5.7.26
--- PHP Version: 7.3.8
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 11 jan. 2022 à 15:44
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Database: `rugby`
+-- Base de données : `rcvb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `adherents`
+-- Structure de la table `adherents`
 --
 
-CREATE TABLE `adherents` (
-  `idAdh` int(4) NOT NULL,
+DROP TABLE IF EXISTS `adherents`;
+CREATE TABLE IF NOT EXISTS `adherents` (
+  `idAdh` int(4) NOT NULL AUTO_INCREMENT,
   `nom` varchar(40) COLLATE utf8_bin NOT NULL,
   `prenom` varchar(40) COLLATE utf8_bin NOT NULL,
   `dateNaiss` date NOT NULL,
@@ -36,35 +44,43 @@ CREATE TABLE `adherents` (
   `numSS` char(15) COLLATE utf8_bin NOT NULL,
   `idP` int(5) NOT NULL,
   `pieceIdentite` longblob NOT NULL,
-  `photo` blob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `photo` blob NOT NULL,
+  `idCat` varchar(4) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`idAdh`),
+  KEY `fk_idParent` (`idP`),
+  KEY `idCat` (`idCat`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `adherents`
+-- Déchargement des données de la table `adherents`
 --
 
-INSERT INTO `adherents` (`idAdh`, `nom`, `prenom`, `dateNaiss`, `lieuNaiss`, `adresse`, `ville`, `cp`, `telPortable`, `telDomicile`, `mail`, `nationalite`, `numSS`, `idP`, `pieceIdentite`, `photo`) VALUES
-(12, 'nhvnhvmbmb', 'jhvnhv', '2000-09-09', 'Paris 11', 'nbv sfv', 'Paris 15e Arrondissement', '75015', '0643908745', '0982736455', 'hgdf@gmail.com', 'francaise', '12345678901234', 4, '', ''),
-(13, 'fatou', 'guirassy', '2009-09-08', 'paris 11', '65 boulevard victor', 'paris', '75015', '0709090909', '', 'fcdgd@gouj.fr', 'francaise', '121234567890123', 5, '', ''),
-(14, 'fatou', 'guirassy', '2009-09-08', 'paris 11', '65 boulevard victor', 'paris', '75015', '0709090909', '', 'fcdgd@gouj.fr', 'francaise', '121234567890123', 6, '', ''),
-(15, 'fatou', 'guirassy', '2009-09-08', 'paris 11', '65 boulevard victor', 'paris', '75015', '0709090909', '', 'fcdgd@gouj.fr', 'francaise', '121234567890123', 7, '', '');
+INSERT INTO `adherents` (`idAdh`, `nom`, `prenom`, `dateNaiss`, `lieuNaiss`, `adresse`, `ville`, `cp`, `telPortable`, `telDomicile`, `mail`, `nationalite`, `numSS`, `idP`, `pieceIdentite`, `photo`, `idCat`) VALUES
+(12, 'nhvnhvmbmb', 'jhvnhv', '2000-09-09', 'Paris 11', 'nbv sfv', 'Paris 15e Arrondissement', '75015', '0643908745', '0982736455', 'hgdf@gmail.com', 'francaise', '12345678901234', 4, '', '', NULL),
+(13, 'fatou', 'guirassy', '2009-09-08', 'paris 11', '65 boulevard victor', 'paris', '75015', '0709090909', '', 'fcdgd@gouj.fr', 'francaise', '121234567890123', 5, '', '', NULL),
+(14, 'fatou', 'guirassy', '2009-09-08', 'paris 11', '65 boulevard victor', 'paris', '75015', '0709090909', '', 'fcdgd@gouj.fr', 'francaise', '121234567890123', 6, '', '', NULL),
+(15, 'fatou', 'guirassy', '2009-09-08', 'paris 11', '65 boulevard victor', 'paris', '75015', '0709090909', '', 'fcdgd@gouj.fr', 'francaise', '121234567890123', 7, '', '', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categorie`
+-- Structure de la table `categorie`
 --
 
-CREATE TABLE `categorie` (
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
   `idCat` varchar(4) COLLATE utf8_bin NOT NULL,
   `nomCat` varchar(20) COLLATE utf8_bin NOT NULL,
   `idResp` int(2) DEFAULT NULL,
   `idEntrn` int(2) DEFAULT NULL,
-  `prix` float NOT NULL
+  `prix` float NOT NULL,
+  PRIMARY KEY (`idCat`),
+  KEY `fk_responsable` (`idResp`),
+  KEY `fk_entraineur` (`idEntrn`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `categorie`
+-- Déchargement des données de la table `categorie`
 --
 
 INSERT INTO `categorie` (`idCat`, `nomCat`, `idResp`, `idEntrn`, `prix`) VALUES
@@ -85,21 +101,23 @@ INSERT INTO `categorie` (`idCat`, `nomCat`, `idResp`, `idEntrn`, `prix`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dirigeant`
+-- Structure de la table `dirigeant`
 --
 
-CREATE TABLE `dirigeant` (
-  `idDirigeant` int(2) NOT NULL,
+DROP TABLE IF EXISTS `dirigeant`;
+CREATE TABLE IF NOT EXISTS `dirigeant` (
+  `idDirigeant` int(2) NOT NULL AUTO_INCREMENT,
   `nom` varchar(40) COLLATE utf8_bin NOT NULL,
   `prenom` varchar(40) COLLATE utf8_bin NOT NULL,
   `login` varchar(35) COLLATE utf8_bin NOT NULL,
   `mdp` varchar(35) COLLATE utf8_bin NOT NULL,
   `mail` varchar(255) COLLATE utf8_bin NOT NULL,
-  `telPortable` char(10) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `telPortable` char(10) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`idDirigeant`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `dirigeant`
+-- Déchargement des données de la table `dirigeant`
 --
 
 INSERT INTO `dirigeant` (`idDirigeant`, `nom`, `prenom`, `login`, `mdp`, `mail`, `telPortable`) VALUES
@@ -108,20 +126,24 @@ INSERT INTO `dirigeant` (`idDirigeant`, `nom`, `prenom`, `login`, `mdp`, `mail`,
 -- --------------------------------------------------------
 
 --
--- Table structure for table `entrainements`
+-- Structure de la table `entrainements`
 --
 
-CREATE TABLE `entrainements` (
-  `idEntrainement` int(2) NOT NULL,
+DROP TABLE IF EXISTS `entrainements`;
+CREATE TABLE IF NOT EXISTS `entrainements` (
+  `idEntrainement` int(2) NOT NULL AUTO_INCREMENT,
   `jours` varchar(20) COLLATE utf8_bin NOT NULL,
   `debut` char(5) COLLATE utf8_bin NOT NULL,
   `fin` char(5) COLLATE utf8_bin NOT NULL,
   `idTerrain` int(2) NOT NULL,
-  `idCat` varchar(5) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `idCat` varchar(5) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`idEntrainement`),
+  KEY `fk_terrains` (`idTerrain`),
+  KEY `fk_categories` (`idCat`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `entrainements`
+-- Déchargement des données de la table `entrainements`
 --
 
 INSERT INTO `entrainements` (`idEntrainement`, `jours`, `debut`, `fin`, `idTerrain`, `idCat`) VALUES
@@ -149,19 +171,21 @@ INSERT INTO `entrainements` (`idEntrainement`, `jours`, `debut`, `fin`, `idTerra
 -- --------------------------------------------------------
 
 --
--- Table structure for table `entraineurs`
+-- Structure de la table `entraineurs`
 --
 
-CREATE TABLE `entraineurs` (
-  `idEntrn` int(2) NOT NULL,
+DROP TABLE IF EXISTS `entraineurs`;
+CREATE TABLE IF NOT EXISTS `entraineurs` (
+  `idEntrn` int(2) NOT NULL AUTO_INCREMENT,
   `nom` varchar(40) COLLATE utf8_bin NOT NULL,
   `prenom` varchar(40) COLLATE utf8_bin NOT NULL,
   `mail` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `telephone` char(10) COLLATE utf8_bin DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `telephone` char(10) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`idEntrn`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `entraineurs`
+-- Déchargement des données de la table `entraineurs`
 --
 
 INSERT INTO `entraineurs` (`idEntrn`, `nom`, `prenom`, `mail`, `telephone`) VALUES
@@ -176,23 +200,28 @@ INSERT INTO `entraineurs` (`idEntrn`, `nom`, `prenom`, `mail`, `telephone`) VALU
 -- --------------------------------------------------------
 
 --
--- Table structure for table `inscrits`
+-- Structure de la table `message`
 --
 
-CREATE TABLE `inscrits` (
-  `idInscrit` int(2) NOT NULL,
-  `idAdh` int(2) NOT NULL,
-  `idCat` varchar(4) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE IF NOT EXISTS `message` (
+  `idMessage` int(11) NOT NULL,
+  `nom` varchar(40) DEFAULT NULL,
+  `prenom` varchar(40) DEFAULT NULL,
+  `mail` varchar(255) DEFAULT NULL,
+  `message` text,
+  PRIMARY KEY (`idMessage`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `parents`
+-- Structure de la table `parents`
 --
 
-CREATE TABLE `parents` (
-  `idParent` int(5) NOT NULL,
+DROP TABLE IF EXISTS `parents`;
+CREATE TABLE IF NOT EXISTS `parents` (
+  `idParent` int(5) NOT NULL AUTO_INCREMENT,
   `nom` varchar(40) COLLATE utf8_bin NOT NULL,
   `prenom` varchar(40) COLLATE utf8_bin NOT NULL,
   `adresse` varchar(80) COLLATE utf8_bin NOT NULL,
@@ -203,17 +232,18 @@ CREATE TABLE `parents` (
   `telDom` char(10) COLLATE utf8_bin NOT NULL,
   `mail` char(255) COLLATE utf8_bin NOT NULL,
   `lienParente` varchar(15) COLLATE utf8_bin NOT NULL,
-  `pieceIdentiteP` longblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `pieceIdentiteP` longblob NOT NULL,
+  PRIMARY KEY (`idParent`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `parents`
+-- Déchargement des données de la table `parents`
 --
 
 INSERT INTO `parents` (`idParent`, `nom`, `prenom`, `adresse`, `profession`, `ville`, `cp`, `telPor`, `telDom`, `mail`, `lienParente`, `pieceIdentiteP`) VALUES
 (1, 'guirassy', 'fgbgfb', 'dxfgsfgh', 'trsghsrtg', 'ertsr', '75007', '0745434555', '', 'fagsn@jnn.fr', '', ''),
-(2, 'cvgvh', 'bis', 'jugvfc', 'ratp', 'paris', '94500', '07454345667', '', 'mnhbvg@jhb.fr', 'mere', ''),
-(3, 'cvgvh', 'bis', 'jugvfc', 'ratp', 'paris', '94500', '07454345667', '', 'mnhbvg@jhb.fr', 'pere', ''),
+(2, 'cvgvh', 'bis', 'jugvfc', 'ratp', 'paris', '94500', '0745434566', '', 'mnhbvg@jhb.fr', 'mere', ''),
+(3, 'cvgvh', 'bis', 'jugvfc', 'ratp', 'paris', '94500', '0745434566', '', 'mnhbvg@jhb.fr', 'pere', ''),
 (4, 'hjdvc', 'sdfszge', '65 Boulevard Victor', 'bgcf', 'Paris 15e Arrondissement', '75015', '0790909090', '', 'jzshg@htmail.com', 'soeur', ''),
 (5, 'hgfc', 'jmbhv', 'jhv hv', 'nbsdc', 'paris', '75009', '0702983899', '', 'sad@jhsd.fr', 'frere', ''),
 (6, 'hgfc', 'jmbhv', 'jhv hv', 'nbsdc', 'paris', '75009', '0702983899', '', 'sad@jhsd.fr', 'bpere', ''),
@@ -223,194 +253,86 @@ INSERT INTO `parents` (`idParent`, `nom`, `prenom`, `adresse`, `profession`, `vi
 -- --------------------------------------------------------
 
 --
--- Table structure for table `responsable`
+-- Structure de la table `responsable`
 --
 
-CREATE TABLE `responsable` (
+DROP TABLE IF EXISTS `responsable`;
+CREATE TABLE IF NOT EXISTS `responsable` (
   `idResp` int(2) NOT NULL,
   `nom` varchar(40) COLLATE utf8_bin NOT NULL,
   `prenom` varchar(40) COLLATE utf8_bin NOT NULL,
   `mail` varchar(255) COLLATE utf8_bin NOT NULL,
   `telephone` char(10) COLLATE utf8_bin DEFAULT NULL,
-  `poste` varchar(30) COLLATE utf8_bin NOT NULL
+  `poste` varchar(30) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`idResp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `responsable`
+-- Déchargement des données de la table `responsable`
 --
 
-INSERT INTO `responsable` (`idResp`, `nom`, `prenom`, `mail`, `telephone`) VALUES
-(1, 'Zellner', 'Sylvie', 'sylvief94800@hotmail.fr', '0637590313'),
-(2, 'Lefranc', 'Anthony', 'lefranc.anthony94@gmail.com', '0661981067'),
-(3, 'Guglielminetti', 'Sophie', 'guglielminetti.sophie@neuf.fr', ''),
-(4, 'Corneille', 'Aurelie', 'aurelie94800@hotmail.com', ''),
-(5, 'Lucas', 'Elisabeth', 'bbth94550@hotmail.com', '0761621503');
+INSERT INTO `responsable` (`idResp`, `nom`, `prenom`, `mail`, `telephone`, `poste`) VALUES
+(1, 'Zellner', 'Sylvie', 'sylvief94800@hotmail.fr', '0637590313', ''),
+(2, 'Lefranc', 'Anthony', 'lefranc.anthony94@gmail.com', '0661981067', ''),
+(3, 'Guglielminetti', 'Sophie', 'guglielminetti.sophie@neuf.fr', '', ''),
+(4, 'Corneille', 'Aurelie', 'aurelie94800@hotmail.com', '', ''),
+(5, 'Lucas', 'Elisabeth', 'bbth94550@hotmail.com', '0761621503', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `terrains`
+-- Structure de la table `terrains`
 --
 
-CREATE TABLE `terrains` (
-  `idTerrain` int(2) NOT NULL,
+DROP TABLE IF EXISTS `terrains`;
+CREATE TABLE IF NOT EXISTS `terrains` (
+  `idTerrain` int(2) NOT NULL AUTO_INCREMENT,
   `nomTerrain` char(50) COLLATE utf8_bin NOT NULL,
   `ville` varchar(30) COLLATE utf8_bin NOT NULL,
   `cp` char(5) COLLATE utf8_bin NOT NULL,
   `adresse` varchar(80) COLLATE utf8_bin NOT NULL,
-  `transport` varchar(100) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `transport` varchar(100) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`idTerrain`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `terrains`
+-- Déchargement des données de la table `terrains`
 --
 
-INSERT INTO `terrains` (`idTerrain`, `nomTerrain`) VALUES
-(1, 'Stade Guy Boniface - Villejuif'),
-(2, 'Stade de L\'Hayette - L\'Haÿ les Roses'),
-(3, 'Stade Evelyne Gérard'),
-(4, 'Stade Alfan Port Talbot'),
-(5, 'Pelouse Parc des Hautes Bruyères');
+INSERT INTO `terrains` (`idTerrain`, `nomTerrain`, `ville`, `cp`, `adresse`, `transport`) VALUES
+(1, 'Stade Guy Boniface - Villejuif', '', '', '', ''),
+(2, 'Stade de L\'Hayette - L\'Haÿ les Roses', '', '', '', ''),
+(3, 'Stade Evelyne Gérard', '', '', '', ''),
+(4, 'Stade Alfan Port Talbot', '', '', '', ''),
+(5, 'Pelouse Parc des Hautes Bruyères', '', '', '', '');
 
 --
--- Indexes for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Indexes for table `adherents`
+-- Contraintes pour la table `adherents`
 --
 ALTER TABLE `adherents`
-  ADD PRIMARY KEY (`idAdh`),
-  ADD KEY `fk_idParent` (`idP`);
-
---
--- Indexes for table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`idCat`),
-  ADD KEY `fk_responsable` (`idResp`),
-  ADD KEY `fk_entraineur` (`idEntrn`);
-
---
--- Indexes for table `dirigeant`
---
-ALTER TABLE `dirigeant`
-  ADD PRIMARY KEY (`idDirigeant`);
-
---
--- Indexes for table `entrainements`
---
-ALTER TABLE `entrainements`
-  ADD PRIMARY KEY (`idEntrainement`),
-  ADD KEY `fk_terrains` (`idTerrain`),
-  ADD KEY `fk_categories` (`idCat`);
-
---
--- Indexes for table `entraineurs`
---
-ALTER TABLE `entraineurs`
-  ADD PRIMARY KEY (`idEntrn`);
-
---
--- Indexes for table `inscrits`
---
-ALTER TABLE `inscrits`
-  ADD PRIMARY KEY (`idInscrit`,`idAdh`,`idCat`),
-  ADD KEY `fk_idAdh` (`idAdh`),
-  ADD KEY `fk_idCat` (`idCat`);
-
---
--- Indexes for table `parents`
---
-ALTER TABLE `parents`
-  ADD PRIMARY KEY (`idParent`);
-
---
--- Indexes for table `responsable`
---
-ALTER TABLE `responsable`
-  ADD PRIMARY KEY (`idResp`);
-
---
--- Indexes for table `terrains`
---
-ALTER TABLE `terrains`
-  ADD PRIMARY KEY (`idTerrain`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `adherents`
---
-ALTER TABLE `adherents`
-  MODIFY `idAdh` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT for table `dirigeant`
---
-ALTER TABLE `dirigeant`
-  MODIFY `idDirigeant` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `entrainements`
---
-ALTER TABLE `entrainements`
-  MODIFY `idEntrainement` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-
---
--- AUTO_INCREMENT for table `entraineurs`
---
-ALTER TABLE `entraineurs`
-  MODIFY `idEntrn` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `inscrits`
---
-ALTER TABLE `inscrits`
-  MODIFY `idInscrit` int(2) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `parents`
---
-ALTER TABLE `parents`
-  MODIFY `idParent` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `terrains`
---
-ALTER TABLE `terrains`
-  MODIFY `idTerrain` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `adherents`
---
-ALTER TABLE `adherents`
+  ADD CONSTRAINT `adherents_ibfk_1` FOREIGN KEY (`idCat`) REFERENCES `categorie` (`idCat`),
   ADD CONSTRAINT `fk_idParent` FOREIGN KEY (`idP`) REFERENCES `parents` (`idParent`);
 
 --
--- Constraints for table `categorie`
+-- Contraintes pour la table `categorie`
 --
 ALTER TABLE `categorie`
   ADD CONSTRAINT `fk_entraineur` FOREIGN KEY (`idEntrn`) REFERENCES `entraineurs` (`idEntrn`),
   ADD CONSTRAINT `fk_responsable` FOREIGN KEY (`idResp`) REFERENCES `responsable` (`idResp`);
 
 --
--- Constraints for table `entrainements`
+-- Contraintes pour la table `entrainements`
 --
 ALTER TABLE `entrainements`
   ADD CONSTRAINT `fk_categories` FOREIGN KEY (`idCat`) REFERENCES `categorie` (`idCat`),
   ADD CONSTRAINT `fk_terrain` FOREIGN KEY (`idTerrain`) REFERENCES `terrains` (`idTerrain`),
   ADD CONSTRAINT `fk_terrains` FOREIGN KEY (`idTerrain`) REFERENCES `terrains` (`idTerrain`);
+COMMIT;
 
---
--- Constraints for table `inscrits`
---
-ALTER TABLE `inscrits`
-  ADD CONSTRAINT `fk_idAdh` FOREIGN KEY (`idAdh`) REFERENCES `adherents` (`idAdh`),
-  ADD CONSTRAINT `fk_idCat` FOREIGN KEY (`idCat`) REFERENCES `categorie` (`idCat`);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
