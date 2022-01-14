df3384a10a9432e2081c0c1c49e6fea1b1ebfd3c
Ronan <ronantheveu@gmail.com>
2022-01-14 16:57:31 +0100

    creation de notre projet

---
+++ projetv2/index.php
@@ -0,0 +1,34 @@
+<?php
+  // Permet de savoir s'il y a une session. 
+  // C'est-à-dire si un utilisateur s'est connecté à votre site 
+  session_start(); 
+  
+  // Fichier PHP contenant la connexion à votre BDD
+  include('connexionDB.php'); 
+?>
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <title>Accueil</title>
+ </head>
+ <body>
+  <h1>Mon site</h1>
+  <?php
+   if(!isset($_SESSION['id'])){ // Si on ne détecte pas de session alors on verra les liens ci-dessous
+  ?>
+   <a href="formInscription.php">Inscription</a> <!-- Liens de nos futures pages -->
+   <a href="formConnexion.php">Connexion</a>
+  <?php
+    }else{ // Sinon s'il y a une session alors on verra les liens ci-dessous
+  ?>
+   <a href="profil.php">Mon profil</a>
+   <a href="modifier-profil.php">Modifier mon profil</a>
+   <a href="deconnexion.php">Déconnexion</a>
+  <?php
+   }
+  ?>
+ </body>
+</html>
---
+++ projetv2/inscription.php
@@ -0,0 +1,104 @@
+<?php
+  session_start();
+  include('connexionDB.php'); // Fichier PHP contenant la connexion à votre BDD
+ 
+    // S'il y a une session alors on ne retourne plus sur cette page
+    if (isset($_SESSION['id'])){
+        header('Location: index.php');
+        exit;
+    }
+    
+    // Si la variable "$_Post" contient des informations alors on les traitres
+    if(!empty($_POST)){
+        extract($_POST);
+        $valid = true;
+    
+    // On se place sur le bon formulaire grâce au "name" de la balise "input"
+        if (isset($_POST['inscriptionEnfant'])){
+        $nom = (trim($nom));
+        $prenom = (trim($prenom));
+        $mail = (trim($mail));
+        $lieuNaissance = (trim($lieuNaissance));
+        $adresse = (trim($adresse));
+        $ville = (trim($ville));
+        $nationalite = (trim($nationalite));
+
+
+        $extensionPhoto=substr($_FILES['photo']['name'],-4,4);    //prendre l'extension du fichier
+        $extensionIdentite=substr($_FILES['pieceIdentite']['name'],-4,4); //prendre l'extesion du fichier
+
+        $nomCompletIdentite="$nom $prenom$extensionIdentite"; //création du nom de fichier avec nom prenom et l'extension d'origine
+        $_FILES['pieceIdentite']['name']=$nomCompletIdentite;
+
+        $nomCompletphoto="$nom $prenom$extensionPhoto"; //création du nom de fichier avec nom prenom et l'extension d'origine
+        $_FILES['photo']['name']=$nomCompletphoto;
+    
+
+        $destinationP="D:/wamp/www/RCVB.V2/projetv2/images/photo/";
+        $destinationI="D:/wamp/www/RCVB.V2/projetv2/images/pieceIdentite/";
+        $accesI="D:/wamp/www/RCVB.V2/projetv2/".$_FILES['pieceIdentite']['name'];
+        $accesP="D:/wamp/www/RCVB.V2/projetv2/".$_FILES['photo']['name'];
+        
+
+
+
+        if (isset($_FILES['photo']['tmp_name'])) {
+            $photo = copy($_FILES['photo']['tmp_name'], $_FILES['photo']['name']);
+            if($photo) {
+                $source_file = $accesP;
+                $destination_path = $destinationP;
+                rename($source_file, $destination_path . pathinfo($source_file, PATHINFO_BASENAME));
+            }
+        }
+
+        if (isset($_FILES['pieceIdentite']['tmp_name'])) {
+            $pieceIdentite = copy($_FILES['pieceIdentite']['tmp_name'], $_FILES['pieceIdentite']['name']);
+            if($pieceIdentite) {
+                $source_file = $accesI;
+                $destination_path = $destinationI;
+                rename($source_file, $destination_path . pathinfo($source_file, PATHINFO_BASENAME));
+            }
+        }
+        
+
+
+ 
+            // Si toutes les conditions sont remplies alors on fait le traitement
+            if($valid){
+
+
+                // On insert nos données dans la table utilisateur
+                $DB->insert("INSERT INTO adherents ( nom, prenom, dateNaiss, lieuNaiss, adresse, ville, cp, telPortable, telDomicile, mail, nationalite, numSS) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    array($nom, $prenom, $dateNaissance, $lieuNaissance, $adresse, $ville, $cp, $telPortable, $telDomicile, $mail, $nationalite, $numSS));
+                
+                
+                exit;
+            }
+        }
+        if (isset($_POST['inscriptionParent'])){
+            $nom = (trim($nom));
+            $prenom = (trim($prenom));
+            $mail = (trim($mail));
+            $profession = (trim($profession));
+            $adresse = (trim($adresse));
+            $ville = (trim($ville));
+            $lienParente = (trim($lienParente));
+
+
+            // Si toutes les conditions sont remplies alors on fait le traitement
+            if($valid){
+
+
+                // On insert nos données dans la table utilisateur
+                $DB->insert("INSERT INTO parents ( nom, prenom, adresse, ville, cp, telPortable, telDomicile, mail, profession, lienParente) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    array($nom, $prenom, $adresse, $ville, $cp, $telPortable, $telDomicile, $mail, $profession, $lienParente));
+                
+                header('Location: index.php');
+                exit;
+
+
+
+            }
+        }
+    }
+?>
